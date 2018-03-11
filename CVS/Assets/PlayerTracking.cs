9b40df7053c952ed38aa42662c55d2f213aad804
Daniel Lam <RoboLam@users.noreply.github.com>
2018-03-11 12:57:38 -0700

    Sphere Tracking
    BAM!

---
+++ CVS/Assets/PlayerTracking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerTracking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject player = GameObject.Find("Player(Clone)");
+        if (player != null)
+        {
+            Transform playerTransform = player.transform;
+            Vector3 playerPosition = playerTransform.position;
+            GetComponent<NavMeshAgent>().SetDestination(playerPosition);
+        }
+    }
+}
