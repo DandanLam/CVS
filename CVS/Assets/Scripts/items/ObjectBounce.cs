8e1564662da5db3db507fd63c15709b2662d9039
Danny <dannyn6688@gmail.com>
2018-03-28 18:04:34 -0700

    Adding effects

---
+++ CVS/Assets/Scripts/items/ObjectBounce.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectBounce : MonoBehaviour {	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.localPosition = new Vector3 (transform.localPosition.x, Mathf.PingPong(Time.time/5, 0.2f)+0.1f, transform.localPosition.z);
+	}
+}
