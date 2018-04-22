00c2d566a07679eab2c878021ddd327cb93b9963
Daniel Lam <daniel_lam@outlook.com>
2018-04-22 10:43:58 -0700

    cubits bounce off walls

--- CVS/Assets/Scripts/items/cubits.cs
+++ CVS/Assets/Scripts/items/cubits.cs
@@ -6,7 +6,19 @@ using UnityEngine.Networking;
 public class cubits : NetworkBehaviour, IPickUp{
 
     public AudioClip shotSound;
-    
+    Rigidbody myRigidbody;
+    Vector3 oldVel;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        oldVel = myRigidbody.velocity;
+    }
+
     private void Awake()
     {
         if (shotSound != null && gameObject.tag == StringConstants.thorwableTag)
@@ -30,14 +42,28 @@ public class cubits : NetworkBehaviour, IPickUp{
             {
                 health.TakeDamage(-(Health.maxHealth / 2));
             }
-        }else if(hit.tag == StringConstants.sphereTag)
+            NetworkServer.Destroy(gameObject);
+        }
+        else if(hit.tag == StringConstants.sphereTag)
         {
             var health = hit.GetComponent<Health>();
             if(health!= null)
             {
                 health.TakeDamage((Health.maxHealth / 4));
             }
+            NetworkServer.Destroy(gameObject);
+        } else
+        {
+            ContactPoint cp = collision.contacts[0];
+            // calculate with addition of normal vector
+            // myRigidbody.velocity = oldVel + cp.normal*2.0f*oldVel.magnitude;
+
+            // calculate with Vector3.Reflect
+            myRigidbody.velocity = Vector3.Reflect(oldVel, cp.normal);
+
+            // bumper effect to speed up ball
+            //myRigidbody.velocity += cp.normal * 2.0f;
         }
-        NetworkServer.Destroy(gameObject);
+        
     }
 }
