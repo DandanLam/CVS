54df5550b67cf1948d5b88bd809fc7bfcae6d5f6
Danny <dannyn6688@gmail.com>
2018-05-02 17:10:31 -0700

    Created weighted switches with animations

---
+++ CVS/Assets/testButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testButton : MonoBehaviour {
+
+    [SerializeField]
+    private Animator myAnim;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == StringConstants.playerTag)
+        {
+            Debug.Log("player detected");
+            myAnim.SetBool("Pushed", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == StringConstants.playerTag)
+        {
+            Debug.Log("player left");
+            myAnim.SetBool("Pushed", false);
+        }
+
+    }
+}
